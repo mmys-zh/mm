9ee2b171ebb14d0d0b453048a94db54a2eacdf9c
mmys-zh <2577271950@qq.com>
2023-12-30 22:32:14 +0800

    webpack学习2天

---
+++ learns/webpack/simple/vue/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h3>{{ message }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { message: "hello" };
+  },
+};
+</script>
+
+<style>
+h3 {
+  margin: 1em;
+  color: aqua;
+}
+</style>
---
+++ learns/webpack/simple/vue/webpack.config.js
@@ -0,0 +1,43 @@
+// webpack.config.js
+const path = require("path");
+const { VueLoaderPlugin } = require("vue-loader");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devServer: {
+    hot: true,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+      // 它会应用到普通的 `.js` 文件
+      // 以及 `.vue` 文件中的 `<script>` 块
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.ts$/,
+        loader: "ts-loader",
+      },
+      // 它会应用到普通的 `.css` 文件
+      // 以及 `.vue` 文件中的 `<style>` 块
+      {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    // 请确保引入这个插件来施展魔法
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
